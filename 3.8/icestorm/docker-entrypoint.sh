d34cba7edc8fb8deb0d1d198cf230668165617b8
Joe George <joe@zeroc.com>
2025-12-18 14:45:04 -0500

    Fix

--- 3.8/icestorm/docker-entrypoint.sh
+++ 3.8/icestorm/docker-entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 [ -e /data ] || mkdir /data
 
 if [ "${1:0:1}" = '-' ]; then
-	set -- icebox --IceBox.Service.IceStorm=IceStormService,37:createIceStorm --IceStorm.LMDB.Path=/data "$@"
+	set -- icebox --IceBox.Service.IceStorm=IceStormService,38:createIceStorm --IceStorm.LMDB.Path=/data "$@"
 fi
 
 if [ "$1" = "icebox" ]; then
